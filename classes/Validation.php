1c098dfb63574bff268aac72ea62d4ec5d2b9662
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2023-03-02 17:22:12 +0300

    Added RegEx patterns array, state types array, and validation method to the validation class. The method does a status check first. If the check return is true, the method takes the object and matches the RegEx patterns, return object if it matches, and returns an error if not.

--- classes/Validation.php
+++ classes/Validation.php
@@ -2,5 +2,44 @@
 
 class Validation 
 {
+    private $patterns = array(
+        "name" => '/[a-zA-Z].{0,254}/',
+        "startDate" => '/[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}/',
+        "endDate" => '/[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}|null/',
+        "duration" => '/[a-zA-Z0-9|:|.|\/s].*|null/',
+        "durationUnit" => '/^(HOURS|DAYS|WEEKS)$\b|null/',
+        "color" => '/\#[a-zA-Z0-9].{0,5}|null/',
+        "externalId" => '/[a-zA-Z0-9].{0,254}/',
+    );
     
+    private $status = array(
+        "NEW",
+        "PLANNED",
+        "DELETED"
+    );
+
+    public function validate($object)
+    {
+        foreach($this->patterns as $pattern_key => $pattern)
+        {
+            foreach($object as $object_key => $value)
+            {
+                if($object_key == "status")
+                {
+                    if(!in_array($value, $this->status))
+                    {
+                        return "Invalid status value!";
+                    }
+                }
+                if($pattern_key == $object_key && $value != "")
+                {
+                    if(!preg_match($pattern, $value))
+                    {
+                        return "Invalid value: " . $object_key . ": " . $value;
+                    }
+                }
+            }
+        }
+        return $object;
+    }
 }
